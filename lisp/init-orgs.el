cc9c83efcc470ac8f088827f4e7cc41660b15789
Jack <loveminimal@163.com>
2019-01-16 21:54:00 +0800

    Define s md exports function.

--- lisp/init-orgs.el
+++ lisp/init-orgs.el
@@ -17,11 +17,18 @@
 
 
 
-
-
-
-
-
+(defun md-export ()
+  "Create front-matter of GFM-md.
+Note that it just adapts to a title including most five words."
+  (interactive)
+  (setq fname (car (split-string (buffer-name) "\\."))) ;; if current buffer is "hello-world.org", FNAME will be "hello-world"
+  (setq fnamel (split-string fname "-"))		;; Now, fnamel will be ("hello" "world")
+  ;; Split the fnamel and join them with <space>, now ftitle will be "hello world"
+  (setq ftitle (concat "---\ntitle: " (car fnamel) " " (car (cdr fnamel)) " " (car (cdr (cdr fnamel))) " " (car (cdr (cdr (cdr fnamel)))) " " (car (cdr (cdr (cdr (cdr fnamel))))) "\n---\n\n"))
+  (write-region ftitle nil (concat fname ".md")) ;; Add ftitle to a file named "hello-world.md"
+  (org-gfm-export-as-markdown)			 ;; Invoke gfm plugin to open a relative .md buffer
+  (append-to-file nil t (concat fname ".md"))	 ;; Append the contents of "hello-world.md" to "hello-world.md" again
+  (kill-this-buffer))				 ;; Kill the "hello-world.md" generate by GFM plugin to keep you stay in current .org file
 
 
 
