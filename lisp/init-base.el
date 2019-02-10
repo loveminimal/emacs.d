bdd6b83866f12c1a2c87b7465a5c1904c9c3507b
Jack <loveminimal@163.com>
2019-02-10 23:52:50 +0800

    Add xah-dired-sort.

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -158,6 +158,22 @@
 (global-set-key (kbd "M-C-9") (lambda () (interactive) (sanityinc/adjust-opacity nil 2)))
 (global-set-key (kbd "M-C-7") (lambda () (interactive) (modify-frame-parameters nil `((alpha . 100)))))
 
+;; @Xah Lee
+(defun xah-dired-sort ()
+  "Sort dired dir listing in different ways.
+Prompt for a choice.
+URL `http://ergoemacs.org/emacs/dired_sort.html'
+Version 2018-12-23"
+  (interactive)
+  (let ($sort-by $arg)
+    (setq $sort-by (ido-completing-read "Sort by:" '( "date" "size" "name" )))
+    (cond
+     ((equal $sort-by "name") (setq $arg "-Al "))
+     ((equal $sort-by "date") (setq $arg "-Al -t"))
+     ((equal $sort-by "size") (setq $arg "-Al -S"))
+     ;; ((equal $sort-by "dir") (setq $arg "-Al --group-directories-first"))
+     (t (error "logic error 09535" )))
+    (dired-sort-other $arg )))
 
 
 (provide 'init-base)
