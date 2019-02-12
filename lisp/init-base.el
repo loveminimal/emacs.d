9f23d82f69e8b493e7760af8e8b1dd2578dbf840
Jack <loveminimal@163.com>
2019-02-12 19:25:42 +0800

    Enhanced iedit mode.

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -55,7 +55,25 @@
 
 
 (global-set-key (kbd "C-.") 'set-mark-command)
+
+;; @Mickey Petersen
 (require 'iedit)
+(defun iedit-dwim (arg)
+  "Starts iedit but uses \\[narrow-to-defun] to limit its scope."
+  (interactive "P")
+  (if arg
+      (iedit-mode)
+    (save-excursion
+      (save-restriction
+        (widen)
+        ;; this function determines the scope of `iedit-start'.
+        (if iedit-mode
+            (iedit-done)
+          ;; `current-word' can of course be replaced by other
+          ;; functions.
+          (narrow-to-defun)
+          (iedit-start (current-word) (point-min) (point-max)))))))
+(global-set-key (kbd "C-;") 'iedit-dwim)
 
 ;; -----------------------------------------------------------------------------
 ;; Some Basic Modes
