1b2094e3c34fd354207918633907c7562570d619
Jack <loveminimal@163.com>
2019-02-20 18:09:58 +0800

    Remove iedit which is not useful.

--- lisp/init-more.el
+++ lisp/init-more.el
@@ -103,29 +103,6 @@
   :diminish)
 
 
-;; @Mickey Petersen
-
-(use-package iedit
-  :ensure t
-  :config
-  (defun iedit-dwim (arg)
-    "Starts iedit but uses \\[narrow-to-defun] to limit its scope."
-    (interactive "P")
-    (if arg
-	(iedit-mode)
-      (save-excursion
-	(save-restriction
-          (widen)
-          ;; this function determines the scope of `iedit-start'.
-          (if iedit-mode
-              (iedit-done)
-            ;; `current-word' can of course be replaced by other
-            ;; functions.
-            (narrow-to-defun)
-            (iedit-start (current-word) (point-min) (point-max)))))))
-  (global-set-key (kbd "C-;") 'iedit-dwim))
-
-
 
 
 
