b702d4f327d4bd1ddd163203c8a386881f64d437
Jack Liu <loveminimal@163.com>
2019-11-12 14:31:06 +0800

    Simplify.

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -11,7 +11,7 @@
 
 (setq-default
  initial-scratch-message (concat ";; Happy hacking, Gnu emacs :)\n\n")
- ;; line-spacing 0.1
+ line-spacing 0.1
  truncate-lines t
  word-wrap t)
 
@@ -25,6 +25,7 @@
 
 
 (display-time)
+(setq display-time-24hr-format t)
 (fset 'yes-or-no-p 'y-or-n-p)
 (delete-selection-mode t)
 (defalias 'list-buffers 'ibuffer)
@@ -189,23 +190,6 @@
 (global-set-key (kbd "M-C-9") (lambda () (interactive) (sanityinc/adjust-opacity nil 2)))
 (global-set-key (kbd "M-C-7") (lambda () (interactive) (modify-frame-parameters nil `((alpha . 100)))))
 
-
-;; @Xah Lee
-(defun xah-dired-sort ()
-  "Sort dired dir listing in different ways.
-Prompt for a choice.
-URL `http://ergoemacs.org/emacs/dired_sort.html'
-Version 2018-12-23"
-  (interactive)
-  (let ($sort-by $arg)
-    (setq $sort-by (ido-completing-read "Sort by:" '( "date" "size" "name" )))
-    (cond
-     ((equal $sort-by "name") (setq $arg "-Al "))
-     ((equal $sort-by "date") (setq $arg "-Al -t"))
-     ((equal $sort-by "size") (setq $arg "-Al -S"))
-     ;; ((equal $sort-by "dir") (setq $arg "-Al --group-directories-first"))
-     (t (error "logic error 09535" )))
-    (dired-sort-other $arg )))
 
 
 (provide 'init-base)
