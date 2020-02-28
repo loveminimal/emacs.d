392b7094615f61214d9f7301ceca7b15e38ae703
Jack Liu <loveminimal@163.com>
2020-02-28 12:38:20 +0800

    Add usage of Macro.

--- macro.el
+++ macro.el
@@ -1,5 +1,15 @@
 ;;; macro.el --- Define some macros.
 ;;; Commentary:
+
+;; | Key/Command               | Description                  |
+;; |---------------------------+------------------------------|
+;; | =C-x (=                   | 开启宏记录                   |
+;; | =C-x )=                   | 关闭宏记录                   |
+;; | =C-x e=                   | 执行刚录制的宏               |
+;; | =C-u n C-x e=             | 执行 n 次刚录制的宏          |
+;; | =M-x name-last-kbd-marco= | 给刚记录的宏命名             |
+;; | =M-x insert-kbd-marco=    | 把刚命名的宏记录写入到文件中 |
+
 ;;; Code:
 
 
@@ -9,9 +19,5 @@
 
 
 
-
-
-
-
 (provide 'macro)
-;;; init.el ends here
+;;; macro.el ends here
