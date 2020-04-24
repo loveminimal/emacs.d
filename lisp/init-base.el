f6a85aa3766556c596cb5afbccaaceaf3c49767a
Jack Liu <lovmeinimal@163.com>
2020-04-24 15:53:38 +0800

    Better UI.

--- lisp/init-base.el
+++ lisp/init-base.el
@@ -68,12 +68,12 @@
 ;;     (buffer-face-mode))
 
 ;;   (add-hook 'org-mode-hook 'org-buffer-face-mode-variable))
-;; (when (fboundp 'menu-bar-mode)
-;;   (menu-bar-mode -1))
-;; (when (fboundp 'tool-bar-mode)
-;;   (tool-bar-mode -1))
-;; (when (fboundp 'set-scroll-bar-mode)
-;;   (set-scroll-bar-mode nil))
+(when (fboundp 'menu-bar-mode)
+  (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'set-scroll-bar-mode)
+  (set-scroll-bar-mode nil))
 
 
 ;; Keybindings without extra configuration
