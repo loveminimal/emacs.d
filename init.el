cc730a1b4911ecf102b0d778d640ea5435d364d5
Jack Liu <loveminimal@163.com>
2021-07-08 20:34:34 +0800

    Better garbage collection.

--- init.el
+++ init.el
@@ -5,6 +5,15 @@
 (setq debug-on-error t)
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+;; Adjust garbage collection thresholds during startup, and thereafter
+
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (setq macro-file (expand-file-name "macro.el" user-emacs-directory))
 
