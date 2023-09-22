df676f2fb68e29ec5fbefa1f3407a2761561a9ce
jack <loveminimal@163.com>
2023-09-22 10:51:14 +0800

    Keep custom.el.

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org-download sort-tab abc-mode quelpa-use-package quelpa youdao-dictionary yasnippet writeroom-mode which-key web-mode use-package-chords switch-window swiper smex simple-httpd ripgrep rainbow-delimiters projectile pangu-spacing move-dup monokai-theme markdown-mode ido-vertical-mode ibuffer-sidebar htmlize expand-region evil-surround evil-leader emmet-mode edit-indirect dired-sidebar diminish delight company-emoji auto-complete)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(Info-quoted ((t (:inherit fixed-pitch-serif :family "LXGW WenKai Mono"))))
+ '(fixed-pitch ((t (:family "LXGW WenKai Mono"))))
+ '(fixed-pitch-serif ((t (:family "LXGW WenKai Mono"))))
+ '(help-key-binding ((t (:inherit fixed-pitch :background "grey19" :foreground "LightBlue" :box (:line-width (-1 . -1) :color "grey35") :family "LXGW WenKai Mono"))))
+ '(markdown-code-face ((t (:inherit fixed-pitch :family "LXGW WenKai Mono"))))
+ '(read-multiple-choice-face ((t (:inherit (help-key-binding underline) :weight bold :family "LXGW WenKai Mono"))))
+ '(tab-bar ((t (:inherit variable-pitch :background "systembuttonface" :foreground "systembuttontext" :family "LXGW WenKai Mono"))))
+ '(tab-line ((t (:inherit variable-pitch :background "grey85" :foreground "black" :height 0.9 :family "LXGW WenKai Mono"))))
+ '(variable-pitch ((t (:foundry "outline" :family "LXGW WenKai Mono")))))
